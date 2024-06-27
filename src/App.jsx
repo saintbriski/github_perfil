788f834fad37b33dcbe30a1e4d52e88fd0ce224c
Willian Briski <wyll_gustavo11@hotmail.com>
2024-06-27 13:32:16 -0300

    tratamento de erro

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,37 @@ import ReposList from "./components/ReposList";
 function App() {
   const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(true);
   const [nomeUsuario, setNomeUsuario] = useState('');
+  const [mostrarTextoInicial, setMostrarTextoInicial] = useState(true);
+  const [erro, setErro] = useState(null);
+
+  const controlaBuscarClique = () => {
+    if (nomeUsuario.length <4 ) {
+      setErro('O nome de usuário deve ter pelo menos 4 caracteres.')
+      setMostrarTextoInicial(false);
+    } else {
+      setErro(null);
+      setMostrarTextoInicial(false);
+    }
+  }
 
     return(
       <>
+      {mostrarTextoInicial && (
+              <div className="texto-inicial">
+              <p>
+                    ENCONTRE REPOSITÓRIOS DIGITANDO O NOME DO USUÁRIO GITHUB
+              </p>
+          </div>
+      )}
       <div className="container-pesquisa">
         <div className="inputbox">
           <input type="text" placeholder="Nome do usuário" onBlur={(e) => setNomeUsuario(e.target.value)} />
           <i></i>
         </div>
-          <button> <span>Buscar</span></button>
+          <button onClick={controlaBuscarClique}> <span>Buscar</span></button>
+      </div>
+      <div className="erro-msg">
+          {erro && <p className="erro-message">{erro}</p>}
       </div>
 
       {nomeUsuario.length > 4 && (
